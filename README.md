11fe763cf79d52eda3cfbca7ceb431d803d89b76
Dylan Smith <dylantsmith@gmail.com>
2019-05-30 21:25:10 -0500

    Update table, include more seed data
    - Update the table for Cats
    - Add a few rows of seed data

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # catsdb
+
+All about registering cats. And logging them in. And then for fun, search and random. Cause, well, I guess why not?
+
+## Getting Started
+
+Install Node (I recommend using [nvm](https://github.com/creationix/nvm)) and [yarn](https://yarnpkg.com). Specific versions of Node and Yarn are required, and can be found in the `package.json` file.
+
+Install the dependencies:
+
+```
+$ yarn
+```
+
+This app requires a running instance of mysql. To seed the database, run the following command:
+
+```
+$ yarn db:seed
+```
+
+To start the application, run the following command:
+
+```
+$ yarn start
+```
+
+## Tests
+
+Tests are written along side the source files, and include `.test.` in the file name. To execute the tests, run the following command:
+
+```
+$ yarn test
+```
+
+To run the tests in watch mode:
+
+```
+$ yarn test:watch
+```
--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,23 @@
 DROP DATABASE IF EXISTS `catsdb`;
-CREATE DATABASE IF NOT EXISTS `catsdb`;
+CREATE DATABASE `catsdb`;
 
 USE catsdb;
 CREATE TABLE `Cats`(
   `id` BIGINT(20) NOT NULL,
-  `name` VARCHAR(255) DEFAULT NULL,
-  `addedAt` TIMESTAMP,
-  `lastSeenAt` TIMESTAMP
+  `name` VARCHAR(255) NOT NULL,
+  `addedAt` TIMESTAMP NOT NULL,
+  `breed` VARCHAR(255),
+  `birthdate` DATE,
+  `imageUrl` VARCHAR(255),
+  `lastSeenAt` TIMESTAMP NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `username` VARCHAR(255) NOT NULL,
+  `weight` FLOAT NOT NULL
 );
 
-INSERT INTO `Cats`(`id`, `name`, `addedAt`, `lastSeenAt`)
+INSERT INTO `Cats`
+  (`id`, `name`, `addedAt`, `breed`, `birthdate`, `imageUrl`, `lastSeenAt`, `password`, `username`, `weight`)
 VALUES
-  (1, 'Meow', NOW(), NOW());
+  (1, 'Meow', NOW(), 'Yellow', '2018-08-01', 'https://placekitten.com/200/300',  NOW(), 'me0w', 'meow', 22),
+  (1, 'Purr', NOW(), 'Gray', '2012-01-23', 'https://placekitten.com/200/300',  NOW(), 'puRR', 'purr', 19.1),
+  (1, 'Hiss', NOW(), 'Orange', '2015-05-20', 'https://placekitten.com/200/300',  NOW(), 'hi55', 'hiss', 15.2);
